671b04ac9e0d6fca1710dc66405b9383b87d80de
Olivier Dacre-Wright <Rouilleur@users.noreply.github.com>
2019-05-08 19:57:49 +0200

    fic broker url (only clients were updated)

--- broker/src/main/java/com/rouilleur/jmsdockerpoc/broker/config/JmsBrokerConfig.java
+++ broker/src/main/java/com/rouilleur/jmsdockerpoc/broker/config/JmsBrokerConfig.java
@@ -27,7 +27,7 @@ public class JmsBrokerConfig {
     @Bean
     public BrokerService broker() throws Exception {
         BrokerService broker = new BrokerService();
-        broker.addConnector("tcp://localhost:61616");
+        broker.addConnector(BROKER_URL);
         return broker;
     }
 
